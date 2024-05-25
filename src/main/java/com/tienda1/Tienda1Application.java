4e460c467ee4c160f9ec472d5e37c1d42e4ac43b
kemeldev <kemel.developer@gmail.com>
2024-05-25 10:48:41 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/tienda1/Tienda1Application.java
@@ -0,0 +1,13 @@
+package com.tienda1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda1Application.class, args);
+	}
+
+}
