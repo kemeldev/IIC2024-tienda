c41a26a21ac25b7d39cfecd077b1a5e403d76664
kemeldev <kemel.developer@gmail.com>
2024-06-17 11:18:00 -0600

    Agregadi project config, con varias configuraciones para cambio de idiioma, ademas se agrega controller, para pagina principal y algunas variables

---
+++ src/main/java/com/tienda1/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.tienda1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("saludo", "hola a todos, como estan?");
+        return "index";
+    }
+}
