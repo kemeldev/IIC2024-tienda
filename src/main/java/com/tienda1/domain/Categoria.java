5cba4309683e3dc2a04a6075a464ad4cf0a1f443
kemeldev <kemel.developer@gmail.com>
2024-06-24 09:28:03 -0600

    Packet domain created under com.tienda1, java class categoria created and dependency added for jakarta in POM

---
+++ src/main/java/com/tienda1/domain/Categoria.java
@@ -0,0 +1,32 @@
+package com.tienda1.domain;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.Data;
+import nonapi.io.github.classgraph.json.Id;
+
+@Data
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+    
+    public Categoria() {
+        
+    }
+    
+    public Categoria(String categoria, boolean activo){
+        this.descripcion = categoria;
+        this.activo= activo;
+    }
+    
+}
